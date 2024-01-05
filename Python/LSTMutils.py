e92b894b873ceced9de91cfb3bf1dc276a473ac7
SimonJWard <Simon.J.Ward@outlook.com>
2024-01-04 18:25:26 -0600

    refactored code

--- Python/LSTMutils.py
+++ Python/LSTMutils.py
@@ -2,59 +2,71 @@ import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 import math
+import pandas as pd
+import numpy as np
 
 def MeanVarianceLogLikelyhoodLoss(y_true, y_pred):
-    print
     Variance = tf.expand_dims(y_pred[:,:,1], 2)
-    #Variance = Variance + tf.constant(1e-6, shape=Variance.shape)
     Variance = tf.math.add(Variance, 1e-6)
     Mean = tf.expand_dims(y_pred[:,:,0], 2)
     #tf.math.log is base e by default
     return tf.math.log(Variance)/2 + (y_true-Mean)**2/(2*Variance)
 
+def RedlichPetersonIsotherm(concentration, A, B, beta, I):
+    return I + A*concentration/(1 + B*(concentration**beta))
 
-class LSTMnetwork:
-    "A LSTM network class"
-    def __init__(self,X_train,y_train,validation_split,batch_size,epochs,SequenceLength,verbose):
-        self.X_train = X_train
-        self.y_train = y_train
-        self.validation_split = validation_split
-        self.batch_size = batch_size
-        self.epochs = epochs
+class ExperimentalData:
+    "A class to read and preprocess experimental data"
+    def __init__(self,SequenceLength=None,ExperimentalDataFilePath = "../TrainingData/ExperimentalTrainingSet.csv"
+                 , FEMfitParamsFilePath = "../TrainingData/FEMFitParameters.csv"):
+        
+        self.ExperimentalDataFilePath = ExperimentalDataFilePath
+        self.FEMfitParamsFilePath = FEMfitParamsFilePath
         self.SequenceLength = SequenceLength
-        #'auto', 0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
-        self.verbose = verbose
         
-    def Build(self):
-        self.model = keras.models.Sequential([keras.layers.LSTM(300, input_shape=(self.SequenceLength,1),
-                                                                return_sequences=True, activation='sigmoid')
-                                 , keras.layers.LSTM(100, return_sequences=True, activation='sigmoid')
-                                 , keras.layers.LSTM(2, activation='softplus',return_sequences=True)])
-    
-    def Compile(self):
-        self.model.compile(optimizer="adam",loss = MeanVarianceLogLikelyhoodLoss)
+    def ReadData(self,ConcentrationAsIndex=None):
+        df_full = pd.read_csv(self.ExperimentalDataFilePath,sep=',',header=None,index_col=ConcentrationAsIndex)
         
-    def Train(self):
-        self.history = self.model.fit(self.X_train, self.y_train, validation_split=self.validation_split
-                                      , batch_size=self.batch_size, epochs=self.epochs, verbose=self.verbose)
-
-    def Evaluate(self,X_test,y_test):
-        return self.model.evaluate(X_test, y_test, batch_size=self.batch_size) #min loss shold be -5.988
+        #split data into time converted from seconds to hours, concentration labels and time series data (with/without concentration labels)
+        time = df_full.iloc[0,1:]/3600
+        concentrations = df_full.iloc[1:,0]
+        df_data = df_full.iloc[1:,1:]
+        df_data_with_concentrations = df_full.iloc[1:,0:]
+        return time, concentrations, df_data, df_data_with_concentrations
     
-    def Predict(self,sequence):
-        self.prediction = self.model.predict(sequence)
-        self.X_predict = sequence
+    def ReadFEMfitParams(self):
+        df_full = pd.read_csv(self.FEMfitParamsFilePath,sep=',',header=0)
+        
+        #remove first column of labels
+        df_full = df_full.iloc[:,1:]
+        
+        concentrations = df_full.iloc[:,0]
+        df_data = df_full.iloc[:,1:]
+        return concentrations, df_data
     
-    def PlotPrediction(self):
-        plt.plot(self.prediction.reshape(self.prediction.shape[1],2)[:,0],label="Mean")
-        plt.plot(self.prediction.reshape(self.prediction.shape[1],2)[:,1], label="Variance")
-        plt.plot(self.X_predict.reshape(self.X_predict.shape[1],1), label="Ground Truth")
-        plt.legend()
-    
-    def PlotTraining(self):
-        loss_values = self.history.history['loss']
+    def NormalizeData(self,df_train,df_test,df_val=[]):
+        min_value, max_value = df_train.min().min(), df_train.max().max()
+        df_norm_train = (df_train - min_value)/(max_value - min_value)
+        df_norm_test = (df_test - min_value)/(max_value - min_value)
+        df_norm_val = (df_val - min_value)/(max_value - min_value)
+        return df_norm_train, df_norm_test, df_norm_val
+            
+    def Shape(self,df_norm):
+        X = df_norm.iloc[:,:self.SequenceLength].values
+        y = df_norm.iloc[:,self.SequenceLength-1].values
+        X = np.expand_dims(X, 2)
+        y = np.broadcast_to(y[:,None], (y.shape[0],self.SequenceLength))
+        y = np.expand_dims(y, 2)
+        return X, y
+        
+class ModelTrainingEvaluation:
+    "A class to evaluate model training"
+    def PlotLossHistory(self,history):
+        loss_values = history.history['loss']
+        val_loss_values = history.history['val_loss']
         epochs = range(1, len(loss_values)+1)
         plt.plot(epochs, loss_values, label='Training Loss')
+        plt.plot(epochs, val_loss_values, label='Validation Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.legend()
